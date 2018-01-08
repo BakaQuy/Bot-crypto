b7d41c1db344e82e2f1ff4ae40cbeefc67a0514e
Bakaquy <p.trongquy@gmail.com>
2018-01-08 22:48:14 +0100

    update

--- Main.py
+++ Main.py
@@ -10,26 +10,21 @@ import csv
 import sys
 
 # TWITTER ACCOUNT
+CONSUMER_KEY = "hs14fkRYcc2cG9AQc878v4nM8"
+CONSUMER_SECRET = "7DPdNqq1Ij1e5akNuoLAc5gAQXDHxdQRytD25pC9z0h2lzo1gO"
 
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
+ACCESS_TOKEN = "1289245472-bsF2NmZDVg9hXVpxHV3XfZTBn8osyu9JPXxrZzE"
+ACCESS_TOKEN_SECRET = "keHL91ANXYi6TT2eLK86crnUjwxMd7bKEyCdQUUUxGOGX"
 
 # BITTREX ACCOUNT
-
 API_KEY = ''
 API_SECRET = ''
 
 # SLACK TOKEN
-
-SLACK_TOKEN = ""
+SLACK_TOKEN = "xoxp-290505432145-290505432193-290948857171-ef909d3a120c45cfc5a218ebf77987a2"
 
 # FILES
-
 FOLLOWED_USERS_FILE = 'twitter_doc/twitter_id.csv'
-
 def csv_to_dict(filename):
     with open(filename) as f:
         reader = csv.reader(f)
@@ -37,15 +32,15 @@ def csv_to_dict(filename):
     dico = {item[1]:item[2] for item in data}
     return dico
 
-
 # MAIN
-params_in = [{'epoch':3, 'vol_th':50, 'rate_growth':0.02, 'vol_growth':2},
-            {'epoch':3, 'vol_th':50, 'rate_growth':0.01, 'vol_growth':2},
-            {'epoch':3, 'vol_th':50, 'rate_growth':0.03, 'vol_growth':2}]
+# params_in = [{'epoch':3, 'vol_th':50, 'rate_growth':0.02, 'vol_growth':2},
+#             {'epoch':3, 'vol_th':50, 'rate_growth':0.01, 'vol_growth':2},
+#             {'epoch':3, 'vol_th':50, 'rate_growth':0.03, 'vol_growth':2}]
+
+params_in = [{'epoch':3, 'vol_th':10, 'rate_growth':0.01, 'vol_growth':0.05}]
 
 params_out = [{'period_sma': 14, 'period_ema': 9, 'tick_time': 1, 'period_time':60,
             'take_profit': None, 'stop_loss': None}]
-# associer a une channel slack chaque combinaison de params_in/params_out
 
 
 if __name__ == '__main__':
